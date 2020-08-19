3bb698f942020e4afe05825179e9f6bb119f5977
tkt <tktcorporation.go@gmail.com>
2020-08-19 20:53:51 +0900

    delete box apdding

--- lib/component/increment-button.dart
+++ lib/component/increment-button.dart
@@ -10,10 +10,6 @@ class IncrementButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 56.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.orange),
-      // Row is a horizontal, linear layout.
       child: Row(
         // <Widget> is the type of items in the list.
         children: <Widget>[
