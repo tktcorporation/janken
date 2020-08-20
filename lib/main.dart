8b979193b9b744f264f70eb99eda3d05be98ef91
tkt <tktcorporation.go@gmail.com>
2020-08-20 12:27:40 +0900

    use separated clases

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:janken/battle/battle.dart';
 import 'package:janken/app/storyboard.dart';
+import 'package:janken/template/battle/battle.dart';
 import 'package:storyboard/storyboard.dart';
 
 void main() {
@@ -20,14 +20,6 @@ class MyApp extends StatelessWidget {
       title: '絶対に 1/3 の確率で勝つ',
       theme: ThemeData(
         // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.orange,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
