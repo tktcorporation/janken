e54957616d658872727ffae6b6ff4dcccdafa41e
tkt <tktcorporation.go@gmail.com>
2020-08-23 05:23:31 +0900

    separate an app class from main

---
+++ lib/app/janken.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:janken/domain/counter.dart';
+import 'package:janken/template/battle/battle.dart';
+import 'package:provider/provider.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '絶対に 1/3 の確率で勝つ',
+      theme: ThemeData(
+        // This is the theme of your application.
+        primarySwatch: Colors.orange,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChangeNotifierProvider<Counter>(
+        create: (context) => Counter(),
+        child: BattlePage('じゃんけん'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:janken/app/janken.dart';
 import 'package:janken/app/storyboard.dart';
-import 'package:janken/domain/counter.dart';
-import 'package:janken/template/battle/battle.dart';
-import 'package:provider/provider.dart';
 import 'package:storyboard/storyboard.dart';
 
 void main() {
@@ -10,28 +8,7 @@ void main() {
 }
 
 MaterialApp material = MaterialApp(
-    home: Storyboard([
-  ButtonsListStory(),
-]));
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '絶対に 1/3 の確率で勝つ',
-      theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.orange,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ChangeNotifierProvider<Counter>(
-        create: (context) => Counter(),
-        child: BattlePage('じゃんけん'),
-      ),
-    );
-  }
-}
+  home: Storyboard([
+    ButtonsListStory(),
+  ]),
+);
