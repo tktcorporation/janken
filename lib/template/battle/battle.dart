c46f0e87b22b39a572673589d0ef20b4b0a202ca
tkt <tktcorporation.go@gmail.com>
2020-08-23 23:29:58 +0900

    use consumer

--- lib/template/battle/battle.dart
+++ lib/template/battle/battle.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:janken/component/count-text.dart';
 import 'package:janken/component/increment-button.dart';
 import 'package:janken/controller/counterController.dart';
+import 'package:janken/domain/counter.dart';
 import 'package:provider/provider.dart';
 
 class BattlePage extends StatelessWidget {
@@ -10,21 +11,29 @@ class BattlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(this._title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CountText(context
-                .select((CounterController counterc) => counterc.getCount()))
-          ],
+    return ChangeNotifierProvider<CounterController>(
+      create: (_) => CounterController(Counter()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(this._title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Consumer<CounterController>(
+                builder: (ctx, counterController, _) => CountText(
+                  counterController.getCount(),
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: Consumer<CounterController>(
+          builder: (ctx, counterController, _) =>
+              IncrementButton(counterController.increment),
         ),
       ),
-      floatingActionButton:
-          IncrementButton(context.watch<CounterController>().increment),
     );
   }
 }
