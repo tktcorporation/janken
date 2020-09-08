33a83b01604ca4b1a3d8ab7dae05b7ad822ac2ef
tktcorporation <tktcorporation.go@gmail.com>
2020-09-09 03:15:50 +0900

    change position of provider scope

--- lib/app/janken.dart
+++ lib/app/janken.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:janken/component/template/battle/battle.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: '絶対に 1/3 の確率で勝つ',
-      theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.orange,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ProviderScope(
+      child: MaterialApp(
+        title: '絶対に 1/3 の確率で勝つ',
+        theme: ThemeData(
+          // This is the theme of your application.
+          primarySwatch: Colors.orange,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: BattlePage('じゃんけん'),
       ),
-      home: BattlePage('じゃんけん'),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:janken/app/janken.dart';
 import 'package:janken/app/storyboard.dart';
 import 'package:storyboard/storyboard.dart';
 
 void main() {
-  runApp(
-    ProviderScope(
-      child: MyApp()
-    ),
-  );
+  runApp(MyApp());
 }
 
 MaterialApp material = MaterialApp(
